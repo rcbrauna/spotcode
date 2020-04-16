a4a28ae2974762d491d1127c95e43f598d3a58b4
rcbrauna <ryanc.brauna@gmail.com>
2020-04-16 14:13:47 -0300

    rails webpacker and react

---
+++ app/javascript/packs/spotcode.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import App from '../app'
+
+document.addEventListener('turbolinks:load', function() {
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('root'),
+  )
+})
