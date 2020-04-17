34657048ae2b35b7caa52bf8c3de1eea8a1fd0d7
rcbrauna <ryanc.brauna@gmail.com>
2020-04-17 19:57:25 -0300

    superior menu

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import { Button } from "react-bulma-components";
 
-import Routes from './routes'
+import Routes from "./routes";
+import Menu from "./components/common/menu";
 
 const App = () => {
   return (
     <Fragment>
       <BrowserRouter>
+        <Menu />
         <Routes />
       </BrowserRouter>
     </Fragment>
